ecb92c8d119400be02569f6e4bcfefa43e2a31d5
jjzwilliam <jjzwilliam@outlook.com>
2021-09-01 20:36:50 +0100

    debug HOUSING to MMS func; 01B1/Sep

--- model_v3/MODULES/HOUSING_v3.py
+++ model_v3/MODULES/HOUSING_v3.py
@@ -722,12 +722,13 @@ class HOUSING_MODULE:
             self.NH3_flux_pit[day_idx] = 0.0
         else:
             ## pools: from housing to MMS
-            self.manure_pool_to_storage[day_idx] = self.manure_pool[day_idx]
-            self.avail_N_pool_to_storage[day_idx] = (1 - self.daily_Na_decomp_rate[day_idx])*self.avail_N_pool[day_idx]
-            self.resist_N_pool_to_storage[day_idx] = (1 - self.daily_Nr_decomp_rate[day_idx])*self.resist_N_pool[day_idx]
-            self.unavail_N_pool_to_storage[day_idx] = self.unavail_N_pool[day_idx]
-            self.TAN_pool_to_storage[day_idx] = self.TAN_pool[[day_idx]]-self.NH3_flux[day_idx]
-            self.Total_water_pool_to_storage[day_idx] = self.Total_water_pool[day_idx]
+            self.manure_pool_to_storage[day_idx] = self.manure_pool[day_idx]*housing_area
+            self.avail_N_pool_to_storage[day_idx] = (1 - self.daily_Na_decomp_rate[day_idx])*self.avail_N_pool[day_idx]*housing_area
+            self.resist_N_pool_to_storage[day_idx] = (1 - self.daily_Nr_decomp_rate[day_idx])*self.resist_N_pool[day_idx]*housing_area
+            self.unavail_N_pool_to_storage[day_idx] = self.unavail_N_pool[day_idx]*housing_area
+            self.TAN_pool_to_storage[day_idx] = (self.TAN_pool[[day_idx]]-self.NH3_flux[day_idx])*housing_area
+            self.Total_water_pool_to_storage[day_idx] = self.Total_water_pool[day_idx]*housing_area
+            ## NH3 flux is not multiplied by housing area, do this in main script!
             self.NH3_flux_from_barn[day_idx] = self.NH3_flux[day_idx]
 
             self.manure_pool[day_idx] = 0.0
@@ -741,10 +742,10 @@ class HOUSING_MODULE:
             self.NH3_flux[day_idx] = 0.0
 
             if housing_type.lower() == 'barn':
-                self.urea_pool_to_storage[day_idx] = (1 - self.daily_urea_hydro_rate[day_idx])*self.urea_pool[day_idx]
+                self.urea_pool_to_storage[day_idx] = (1 - self.daily_urea_hydro_rate[day_idx])*self.urea_pool[day_idx]*housing_area
                 self.urea_pool[day_idx] = 0.0
             elif housing_type.lower() == 'poultry house':
-                self.UA_pool_to_storage[day_idx] = self.UA_pool[day_idx]
+                self.UA_pool_to_storage[day_idx] = self.UA_pool[day_idx]*housing_area
                 self.UA_pool[day_idx] = 0.0
 
     def housing_to_storage_init(self,housing_type):
